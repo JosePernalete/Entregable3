da7350e38ece8eeb5912fb21b44da7dca1eb4a8c
JosePernalete <josepernalete284@gmail.com>
2021-11-01 17:49:19 -0300

    Primer prototipo

---
+++ Prueba/JuegosOlimpics/src/deportistas.java
@@ -0,0 +1,21 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class deportistas extends JFrame {
+    private JButton nuevoBoton;
+    private JButton exportarCSV;
+    private JButton volver;
+
+    public deportistas() {
+        setSize(500, 500);
+        setResizable(false);
+        setTitle("Gestor de Olimpiadas - DEPORTISTAS");
+        setVisible(true);
+        setLayout(new FlowLayout());
+        nuevoBoton = new JButton("NUEVO");
+        exportarCSV = new JButton("EXPORTAR CSV");
+        volver = new JButton("VOLVER");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
